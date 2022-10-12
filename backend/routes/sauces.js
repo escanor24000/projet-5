7f5a8706f8762fcf9a6dcf28316be2f0751c41fc
escanor24000 <johan.audy.24@gmail.com>
2022-10-12 22:38:58 +0200

    intégration de l'inscription

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', saucesCtrl.createThing);
-router.get('/', saucesCtrl.getAllThings);
-router.get('/:id', saucesCtrl.getOneThing);
-router.put('/:id', saucesCtrl.modifyThing);
-router.delete('/:id', saucesCtrl.deleteThing);
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, saucesCtrl.createThing);
+router.get('/', auth, saucesCtrl.getAllThings);
+router.get('/:id', auth, saucesCtrl.getOneThing);
+router.put('/:id', auth, saucesCtrl.modifyThing);
+router.delete('/:id', auth, saucesCtrl.deleteThing);
 
 router.post('/api/sauces/:id/like', (req, res, next) => {
 
